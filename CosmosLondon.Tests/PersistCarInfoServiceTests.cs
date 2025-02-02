794664d8a23c948290fcfb9624e8c1560e78a6de
Michael <contact@mcrook.com>
2025-02-02 23:57:45 +0800

    v2 created services

--- CosmosLondon.Tests/PersistCarInfoServiceTests.cs
+++ CosmosLondon.Tests/PersistCarInfoServiceTests.cs
@@ -2,14 +2,27 @@ namespace CosmosLondon.Tests;
 
 public class PersistCarInfoServiceTests
 {
+    private IPersistCarInfoService _service; // compile error
+
     [SetUp]
     public void Setup()
     {
+        _service = new PersistCarInfoService(); // compile error
     }
 
     [Test]
-    public void Test1()
+    public void Insert_Should_Exist_Within_PersistCarInfoService()
     {
+        _service.Insert();
+        
         Assert.Pass();
     }
+}
+
+public class PersistCarInfoService : IPersistCarInfoService
+{
+}
+
+internal interface IPersistCarInfoService
+{
 }
